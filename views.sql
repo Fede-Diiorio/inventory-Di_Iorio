f4fe819ac731d4579129a7fa24769f4c245b1898
Federico Di Iorio <federico_metallica@hotmail.com>
2025-02-27 18:02:34 -0300

    feat: view_sales

--- views.sql
+++ views.sql
@@ -1,13 +1,13 @@
 USE coder_inventory;
 
-CREATE OR REPLACE VIEW get_categoires AS
+CREATE OR REPLACE VIEW view_categoires AS
 SELECT
 	id,
     name
 FROM categories
 ORDER BY id;
 
-CREATE OR REPLACE VIEW get_products AS 
+CREATE OR REPLACE VIEW view_products AS 
 SELECT
 	p.id,
     p.name,
@@ -18,3 +18,23 @@ SELECT
 FROM products AS p
 INNER JOIN categories AS c ON (c.id = p.category_id)
 ORDER BY id;
+
+CREATE OR REPLACE VIEW view_sales AS 
+SELECT 
+	s.id,
+    s.date,
+    c.name,
+    c.email,
+    c.phone,
+    SUM(sd.quantity * p.price) AS total
+FROM sales AS s
+INNER JOIN clients AS c ON (c.id = s.client_id)
+INNER JOIN sales_detail AS sd ON (s.id = sd.sale_id)
+INNER JOIN products AS p on (sd.product_id = p.id)
+GROUP BY s.id, s.date, s.client_id
+ORDER BY s.id;
+
+
+
+
+    
