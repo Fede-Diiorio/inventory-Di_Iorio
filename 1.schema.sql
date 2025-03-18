eed0f56e65164edc839954a10c19ef48d264e1ae
Federico Di Iorio <federico_metallica@hotmail.com>
2025-03-18 06:18:38 -0300

    fix: Cambios en las tablas y carácteres

--- 1.schema.sql
+++ 1.schema.sql
@@ -1,21 +1,21 @@
 CREATE SCHEMA IF NOT EXISTS coder_inventory;
 USE coder_inventory;
 
-CREATE TABLE IF NOT EXISTS categories (
+CREATE TABLE IF NOT EXISTS category (
     id INT AUTO_INCREMENT,
     name VARCHAR(40) NOT NULL UNIQUE,
-    CONSTRAINT pk_categories PRIMARY KEY (id)
+    CONSTRAINT pk_category PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS product (
     id INT AUTO_INCREMENT,
     name VARCHAR(60) NOT NULL UNIQUE,
     description TEXT NOT NULL,
     price DECIMAL(10,2) CHECK (price >= 0.01),
     stock INT NOT NULL CHECK (stock >= 0),
     category_id INT NULL,
-    CONSTRAINT pk_products PRIMARY KEY (id),
-    CONSTRAINT fk_products_category FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE SET NULL
+    CONSTRAINT pk_product PRIMARY KEY (id),
+    CONSTRAINT fk_product_category FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS clients (
@@ -26,21 +26,21 @@ CREATE TABLE IF NOT EXISTS clients (
     CONSTRAINT pk_clients PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS sales (
+CREATE TABLE IF NOT EXISTS sale (
     id INT AUTO_INCREMENT,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     client_id INT NOT NULL,
-    CONSTRAINT pk_sales PRIMARY KEY (id),
-    CONSTRAINT fk_sales_client FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE RESTRICT 
+    CONSTRAINT pk_sale PRIMARY KEY (id),
+    CONSTRAINT fk_sale_client FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE RESTRICT 
 );
 
-CREATE TABLE IF NOT EXISTS sales_detail (
+CREATE TABLE IF NOT EXISTS sale_detail (
     sale_id INT NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL DEFAULT 1 CHECK (quantity >= 1),
-    CONSTRAINT pk_sales_detail PRIMARY KEY (sale_id, product_id), 
-    CONSTRAINT fk_sales_detail_sale FOREIGN KEY (sale_id) REFERENCES sales (id) ON DELETE CASCADE,
-    CONSTRAINT fk_sales_detail_product FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE RESTRICT
+    CONSTRAINT pk_sale_detail PRIMARY KEY (sale_id, product_id), 
+    CONSTRAINT fk_sale_detail_sale FOREIGN KEY (sale_id) REFERENCES sale (id) ON DELETE CASCADE,
+    CONSTRAINT fk_sale_detail_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS auditory (
